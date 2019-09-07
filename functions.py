04b270970f8df28f730f40b628c7d1064576e17d
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-07 22:53:08 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,31 @@
+#example 1
+def simple():
+    print("rahulc")
+simple()
+
+#example 2
+def name(name):
+    print("hello "+name+" ..")
+name("rahul")
+
+#example 3
+def sum(num1, num2):
+
+    print(num1+num2)
+sum(10,20)
+
+
+#example 4
+def returnadd(num1, num2):
+    return num1+num2
+
+sum = returnadd(12,20)
+print(sum)
+
+#example 5
+def returnsub(num1,num2):
+    return num1-num2
+sub = returnsub(3,1)
+print(sub)
+
+
