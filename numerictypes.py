370aa6244d0978da33f6284d2c1d00868491a14a
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-08 22:27:06 +0530

    Add files via upload

---
+++ numerictypes.py
@@ -0,0 +1,16 @@
+#integer type
+
+a=10  #consider as int
+b=20  #consider as int
+c=30  #consider as int
+print(a,b,c)  #to print the values using delimeter comma
+
+print(type(a))  #to know what type of variable and here type is inbuilt function in python
+
+#float type
+p=7.5
+q=33.6
+r=12.12
+print(p,q,r)
+
+print(type(r))
