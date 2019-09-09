6dbd608ee1b5987b78e728be35bb9a097dcae83b
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-09 21:27:55 +0530

    Add files via upload

---
+++ stringtypes.py
@@ -0,0 +1,26 @@
+
+s0="awesome "
+print(s0)
+
+s="my name is rahul "  #using string
+print(s)
+
+s1="""you are awesome because   
+your learning python..
+all the best"""  #you can take single or double quotes instead
+print(s1)
+
+s2='i am learning python'    #using single quotes
+print(s2)          
+
+#indexing in strings
+print(s[0])         #output : m
+print(s1[16])         #output : b
+
+#repetition in strings
+print(s*2)       #output : my name is rahul my nameis rahul
+print(s0*3)      #awesome awesome awesome
+
+#finding length of the string
+print(len(s0))   #len is inbuilt function in python so it prints length of the s0 string
+print(len(s1))   #output : 62
