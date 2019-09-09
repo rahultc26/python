c78ac2a0140388f5a2cd4789a4cf495b15db2578
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-09 22:38:35 +0530

    Add files via upload

---
+++ slicingsteps.py
@@ -0,0 +1,14 @@
+#slicing steps in strings
+#for example
+s0="awesome"
+s="my name is Rahul"
+
+#jumping 
+print(s[0:15:2]) #so here specified 2 so it jumps 2 positions
+
+#reverse the string
+print(s[10::-1])     #here reversing the string with number of places specified
+
+print(s[::-1])  #here it is automatically reverse all the string 
+
+
