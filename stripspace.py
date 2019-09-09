a0d814aec6129d92d7c4fe005ee6e3129c0b90d7
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-09 23:40:21 +0530

    Add files via upload

---
+++ stripspace.py
@@ -0,0 +1,14 @@
+#striping the spaces
+#for example
+s="  you are super  "  #add some spaces after and before
+print(s)       #now it prints the string including spaces
+
+#to strip the spaces
+print(s.strip())  #prints without spaces
+
+#to erase left side spaces
+print(s.lstrip())
+
+#to erase right side spaces
+print(s.rstrip())  
+
