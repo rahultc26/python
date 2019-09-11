c47146feb40be95341f0471438b88ea37538ac07
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-11 10:35:29 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+
+#creating empty list
+lst=[]
+print(lst)   #o/p=[]
+
+lst1=[12,2.5,"Rahul",131313]    #list 'lst1' is created  
+print(lst1)
+
+lst2=['rahul']      #you can use single qoutes also
+print(lst2)                    #o/p=['rahul']
+
