99156e0ab5db897c011979f1817455d6e66a7104
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-13 23:50:19 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,32 @@
+#tuple
+#creating Tuple
+tup=(10,20,30,'rc')
+print(tup)
+
+#eg
+tup=(20,)  #if your creating tuple for one value you 
+ #must put comma after value otherwise it will consider as integer type 
+print(tup) 
+
+#eg
+tup=(40)
+print(tup)  #it displays int value 40 now
+
+#finding 
+print(type(tup))  #displays class int;
+
+tup=(20,)
+print(type(tup))  #displays class tuple
+
+#indexing 
+tup=(10,20,50,65,'rc')
+print(tup[2])  #o/p=50
+
+#repetition
+print(tup*2)  #displays contents of tuple twice
+
+#Find count values
+print(tup.count(10))  #o/p=1; because there is only one '10'
+
+#finding index value of value on tuple
+print(tup.index("rc"))  #index value within double quotes;
