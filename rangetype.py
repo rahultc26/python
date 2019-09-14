cb23ff185ba45ab22eb9c0b10f1dc65438c18c6d
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-14 22:36:04 +0530

    Add files via upload

---
+++ rangetype.py
@@ -0,0 +1,16 @@
+#range type within for loop
+r=range(6)
+for i in r:     #One method
+    print(i)    #displays 1 to 5 elements without taking '6'
+    
+#second(easy) method
+for i in range(6):   
+    print(i)     #displays 1 to 5 elements without taking '6'
+    
+    
+#operations
+for i in range(1,15):
+   print(i)        #displays 1 to 14 elements without taking '15'
+    
+for i in range(0,10,2):
+    print(i)  #displays elements from '0' to '10' by skipping elements in order of two
