fed1abfc8d409c75af569d99f09166c28d4a6739
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-23 23:38:18 +0530

    Comparison Operators

---
+++ comparisonoperators.py
@@ -0,0 +1,23 @@
+"""comparison operators
+    ==
+    !=
+    >
+    <
+    >=
+    <= """
+    
+x,y=10,5
+print(x==y)  #false
+
+print(x!=y)  #true
+
+print(x>y)  #true
+
+print(x<y)  #false
+
+#let
+x,y=10,10
+print(x>=y) #true because there is a 'or' operator
+
+print(x<=y)  #also true
+ 
