d114620f6e96e513227a9a61517afaf5220470c5
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-23 19:46:47 +0530

    Operators
    Arithmetic Operators in python

---
+++ arithmeticoperators.py
@@ -0,0 +1,27 @@
+
+a,b=10,5    #value of 10 is assigned to a and 5 is assigned to b
+sum=a+b
+print("addition: ",a+b)
+
+print("subtraction: ",a-b)
+
+print("Multiplication: ",a*b)
+
+print("Div: ",a/b)
+
+print("Mod: ",a%b)
+
+print("Pow: ",a**b)    #10 pow 5 is 100000
+
+print("Floor Div: ",a//b)  #called floor division or integer division it's get adopt the decimal value
+
+#-----------------------------------------------------------#
+
+#Floor division example
+x,y=8,3
+print("Floor div",x//y)     #o/p = 2
+
+
+
+
+
