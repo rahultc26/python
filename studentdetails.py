9cbeb0c2c4fb57109ee70637dd14a4ad91ed4103
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-25 23:02:24 +0530

    Student Information
    Basic python program for student information

---
+++ studentdetails.py
@@ -0,0 +1,6 @@
+Sname=input("Enter Student Name\n")
+USN=input("Enter the USN\n")
+Branch=input("Enter the Branch\n")
+
+print("--Entered details are--")
+print("\nStudent Name:",Sname,"\nStudent USN",USN,"\nStudent Branch",Branch)
