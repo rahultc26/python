ec6c2cf22226a0c6eded7185f284e63f2422408c
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-25 09:23:21 +0530

    Different Types using print

---
+++ printfuns.py
@@ -0,0 +1,22 @@
+print()  #print nothing
+
+print("heyy"*2)
+
+print("this is rahul \n he is studying python") 
+
+a,b=10,25
+print(a,b,sep="-")  #sep is method to use delimeter o/p=10-25
+
+Name="bob"
+marks=99.9
+print(Name,marks)
+
+print("Name is",Name,"\n""Marks are",marks)
+
+print("Name is %s \nMarks are %.2f"%(Name,marks))
+
+print("Name is {} \n Marks are {}".format(Name, marks))
+
+print("Name is {0} \n Marks are {1}".format(Name, marks))
+
+
