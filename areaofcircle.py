20b9967cd6bb5cd52571df1f3c45a40aa5d68cfe
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-26 07:03:01 +0530

    Area of Circle
    Python program for area of a circle

---
+++ areaofcircle.py
@@ -0,0 +1,12 @@
+
+rad=float(input("Enter the radius\n"))
+pi=22/7
+area=pi*rad**2
+print("area of the circle: ",area)
+
+
+#Using modules
+import math  #math is a inbuilt module in python
+rad=float(input("Enter the radius\n"))
+area=math.pi*rad**2
+print("area of the circle: ",area)
