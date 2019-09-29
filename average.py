5febcd7ef326019a7d330bce347d496af2137ec8
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-09-29 15:05:18 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,3 +1,14 @@
 def average(a,b):
     print("Average of",a,"and",b,"is:",(a+b)/2)
 average(100,185)  #you can pass any parameters 
+
+#using return
+def avg(a,b):
+    return (a+b)/2
+result = avg(100,185)
+print(result)
+
+#another way using return
+def avrg(a,b):
+    return "average is",(a+b)/2
+print(avrg(100,185))
