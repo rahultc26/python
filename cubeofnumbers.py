1be5ba211075e655032e36592012934464e107a3
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-01 07:25:01 +0530

    Programs using list comprehensions

---
+++ cubeofnumbers.py
@@ -0,0 +1,11 @@
+'''lst=[]
+lst=[x**3 for x in range(1,11)]
+print(lst) ''' #Note:it is normal form'''
+
+
+list=[]
+list=[x**2 for x in range(1,11)]
+print(list)
+
+m=[x**4 for x in range(1,11)]
+print(m)
---
+++ evennumbers.py
@@ -0,0 +1,11 @@
+#Using list comprehensions
+list=[x for x in range(2,21,2)]
+print(list)
+
+lst=[m for m in range(1,21) if m%2==0]
+print(lst)
+
+'''lst=[y for y in range(2,21,2)]
+print(lst)         its is normal'''
+
+    
