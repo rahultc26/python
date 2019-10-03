c9ce3f46d47a79267e22403f67bd0c19f88386c3
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-03 11:42:43 +0530

    oops
    Creating class in object oriented programming

--- product.py
+++ product.py
@@ -1,3 +1,18 @@
-import sys
-list=sys.argv
-print("product of arguments\n",int(list[1])*int(list[2]))
+class Product:
+    def __init__(self):
+        self.name = 'oneplus7'
+        self.description = 'Its awesome'
+        self.price = 28000
+        
+p1 = Product()
+
+print(p1.name)
+print(p1.description)
+print(p1.price)
+
+p2 = Product()
+
+print(p2.name)
+print(p2.description)
+print(p2.price)
+
