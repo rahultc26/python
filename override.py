9f87d8b44512c37d91e274d4071936a92bedb33b
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-06 14:55:10 +0530

    override by inheriting
    override is used when the super class and subclass want same method names but different statements

---
+++ override.py
@@ -0,0 +1,60 @@
+class Renault:
+    
+    def __init__(self,company,model,year):
+        self.company = company
+        self.model = model
+        self.year = year
+    def start(self):
+        print("starting the engine")
+    def stop(self):
+        print("Stopping the engine")
+    
+        
+class threeseries(Renault):
+    
+    def __init__(self,cruiceControlEnabled,company,model,year):
+        Renault.__init__(self,company,model,year)
+        self.cruiceControlEnabled = cruiceControlEnabled
+    def display(self):
+        print("this is three series car")
+        
+    def start(self):
+        print("this statement is in override method")
+    
+class fiveseries(Renault):
+    
+    def __init__(self,parkingAassistEnabled,company,model,year):
+        Renault.__init__(self,company,model,year)
+        self.parkingAassistEnabled = parkingAassistEnabled
+    def stop(self):
+        print("this statement is in override method")
+    def dis(self):
+        print("this is five series car")
+three = threeseries(True,"Duster","1511","2015")
+print(three.cruiceControlEnabled)
+print(three.company)
+print(three.model)
+print(three.year)
+print("")
+five = fiveseries(True,"kwid","1523","2017")
+print(five.parkingAassistEnabled)
+print(five.company)
+print(five.model)
+print(five.year)
+
+"""---------------------------------------------------------------"""
+#to inherit the method
+three.start()
+three.stop()
+five.stop()
+"""----------------------------------------------------------------"""
+#subclass can define their own functions
+three.display()
+five.dis()
+
+
+
+
+
+
+    
