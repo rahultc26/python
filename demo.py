13e87e69801cd45bd82b0b2b3035a3595dfd4f0f
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-11-09 10:47:21 +0530

    Exception demo example

--- demo.py
+++ demo.py
@@ -1,7 +1,15 @@
-import sys
-list=sys.argv
-for i in list:
-    print(i)
-print(len(list))
- 
-print(list[0])
+try:
+    f = open("myfile","w")
+    a,b = [int(x) for x in input("enter the two numbers\n").split()]
+    c = a/b;
+    print("division ",c)
+    f.write("writing %d into file" %c)
+except ZeroDivisionError:
+    print("ZeroDivision not allowed")
+    print("this is after exception")
+    
+else:
+    print("This is else statement")
+finally:
+    f.close()
+    print("file closed")
